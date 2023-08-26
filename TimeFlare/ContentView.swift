2f6224176652849922a2bf7c0b5fecbf3735108e
Ricardo Sanchez <r.sanchez.macias.dev@gmail.com>
2023-08-25 23:04:29 -0700

    Create TimeFlare project and add AppIcon

---
+++ TimeFlare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TimeFlare
+//
+//  Created by Ricardo Sanchez-Macias on 8/25/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TimeFlare/TimeFlareApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeFlareApp.swift
+//  TimeFlare
+//
+//  Created by Ricardo Sanchez-Macias on 8/25/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeFlareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
